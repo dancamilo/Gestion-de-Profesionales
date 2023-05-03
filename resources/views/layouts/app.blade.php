cf69586d70dc519826960990f62b592a8aefe4c9
dancamilo <mejia.danielcamilo@gmail.com>
2023-05-03 15:59:53 -0600

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,21 +53,21 @@
                    
                 </div>
                 <div class="navbar-nav w-100">
-                    <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i></a>
+                    <a href="index.html" class="nav-item nav-link active"><i class="fa-solid fa-house"></i></a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i></a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-graduation-cap"></i></a>
                         <div class="dropdown-menu bg-transparent border-0">
                             <a href="button.html" class="dropdown-item">vacio</a>
                             <a href="typography.html" class="dropdown-item"></a>
                             <a href="element.html" class="dropdown-item"></a>
                         </div>
                     </div>
-                    <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i></a>
-                    <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i></a>
-                    <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i></a>
-                    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>     </a>
+                    <a href="widget.html" class="nav-item nav-link"><i class="fa-solid fa-user-group"></i></a>
+                    <a href="form.html" class="nav-item nav-link"><i class="fa-solid fa-user-shield"></i></a>
+                    <a href="table.html" class="nav-item nav-link"><i class="fa-solid fa-building"></i><a>
+                    <a href="chart.html" class="nav-item nav-link"><i class="fa-solid fa-file-arrow-up"></i></a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i></a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-magnifying-glass"></i></a>
                         <div class="dropdown-menu bg-transparent border-0">
                             <a href="signin.html" class="dropdown-item">vacio</a>
                             <a href="signup.html" class="dropdown-item"></a>
@@ -96,7 +96,6 @@
                 <div class="navbar-nav align-items-center ms-auto">
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
                             <span class="d-none d-lg-inline-flex">John Doe</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
