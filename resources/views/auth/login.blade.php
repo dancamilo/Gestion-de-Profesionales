36b5be00990acad4a3ed95bd30173c1f3312462b
kevinrodri11 <kevinrodriguez1209p@gmail.com>
2023-05-03 12:52:27 -0500

    "plantilla"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,9 +50,9 @@
                 <div class="col-6 col-sm-12 col-md-12 col-lg-5 col-xl-6">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
                         <div class="d-flex align-items-center justify-content-between mb-3">
-                            <a href="index.html" class="">
-                                <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>DarkPan</h3>
-                            </a>
+                            
+                            <h3 class="text-primary logo" ><i class="fa fa-logo-edit me-2" ></i></h3>
+                            
                             <h3>Sign In</h3>
                         </div>
                         <div class="form-floating mb-3">
