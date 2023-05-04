249b80d1e2ef7f631ce29fd97ea95c393fd35e26
dancamilo <mejia.danielcamilo@gmail.com>
2023-05-04 08:20:35 -0600

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,10 +113,10 @@
 
             <!-- Footer Start -->
             <div class="container-fluid pt-4 px-4">
-                <div class="bg-secondary rounded-top p-4 rounded">
+                <div class="bg-secondary rounded-top p-2  rounded">
                     <div class="row">
-                        <div class="col-12 col-sm-6 text-center text-sm-end footer">
-                        
+                        <div class="col-12 col-sm-6 footer">
+                            <img class="rounded img-footer" src="{{asset('img/Logo1.png')}}" alt="">
                             <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                         </div>
                     </div>
