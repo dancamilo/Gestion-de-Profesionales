b65b9401c45e3160f377b84dd4a0cf030a5dd3df
dancamilo <mejia.danielcamilo@gmail.com>
2023-05-06 13:59:35 -0500

    cambioimagenes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,6 +109,35 @@
         </div>
         <!-- Content End -->
 
+        @section('content')
+
+<!-- Aquí se pueden agregar los elementos que se quieran mostrar en la sección del contenido de la página -->
+
+<!-- Sección para mostrar las seis imágenes -->
+<section id="six-images">
+  <div class="row">
+    <div class="col-md-4 col-12 mb-4">
+       <img class="rounded img-footer" src="{{asset('img/Imgcursos.png')}}" alt="">
+    </div>
+    <div class="col-md-4 col-12 mb-4">
+       <img class="rounded img-footer" src="{{asset('img/Imgprofesional.png')}}" alt="">
+    </div>
+    <div class="col-md-4 col-12 mb-4">
+       <img class="rounded img-footer" src="{{asset('img/Imgaccesousuarios.png')}}" alt="">
+    </div>
+    <div class="col-md-4 col-12 mb-4">
+       <img class="rounded img-footer" src="{{asset('img/Imgempresas.png')}}" alt="">
+    </div>
+    <div class="col-md-4 col-12 mb-4">
+       <img class="rounded img-footer" src="{{asset('img/Imgcertificados.png')}}" alt="">
+    </div>
+    <div class="col-md-4 col-12 mb-4">
+       <img class="rounded img-footer" src="{{asset('img/Imghistorial.png')}}" alt="">
+    </div>
+  </div>
+</section>
+
+@endsection
 
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
