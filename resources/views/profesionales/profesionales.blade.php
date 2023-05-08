e6c7be40ba332828e62a90fffdcadc39ea878e48
LinaCruzM <109240037+LinaCruzM@users.noreply.github.com>
2023-05-08 11:03:27 -0600

    Página profesionales

--- resources/views/profesionales/profesionales.blade.php
+++ resources/views/profesionales/profesionales.blade.php
@@ -18,49 +18,42 @@
                             <option></option>
                         </select>
                     </div>
-                </div>
-
-            <div class="card text-white bg-light mt-5">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <select class="form-select  bg-white text-dark">
-                                <option>Area</option>
-                                <option><div class="card-doby">
-                                            <div class="row">
-                                                <div class="col-sm-12">
-                                                    <h4 class="card-title">Curso 1</h4>
-                                                    <p class="card-text text-dark">Profesional encargado:</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                </option>
-                                <option>3</option>
-                                <option>4</option>
-                            </select>
-                        </div>
-                    </div>
+                    <i class="fa-solid fa-square-plus text-primary col-sm-2"></i>
                 </div>
             </div>
-
-
-
-
-
-            </div>
-
-
-
-
-
-
-
         </div>
-
-
-
-
-
     </div>
 </div>
+<div class="d-flex mt-5">
+    <div class="collapsible overflow-hidden ms-4 ps-2 me-5">
+        <input type="checkbox" id="collapsible-demand rounded-0 mb-2">
+        <label for="collapsible-demand rounded-0 mb-2" >Perfil 1<i class="fa-solid fa-chevron-down"></i></label>
+        <div class="collapsible-text text-dark rounded-0 p-4 pb-0">
+            <div class=" w-100 h-100 ms-0 border-left d-flex">
+                <div class="p-4">
+                    <h3>Nombre</h3>
+                    <h5>Curso 1</h5>
+                    <a href="#">nombre@uniempresarial.edu.co</a>  
+                </div>
+                <div>
+                    <img class="rounded-circle" src="{{asset('img/User1.jpg')}}" alt="">
+                </div>
+            </div>
+            <hr class=".hr">
+            <div class=" w-100 h-100 ms-0 mt-2 border-left d-flex">
+                <div class="p-4">
+                    <h3>Nombre</h3>
+                    <h5>Curso 4</h5>
+                    <a href="#">nombre@uniempresarial.edu.co</a>  
+                </div>
+                <div>
+                    <img class=" img2 rounded-circle" src="{{asset('img/User2.jpg')}}" alt="">
+                </div>
+            </div>
+            <hr>
+        </div>
+    </div>
+    
+</div>
+
 @endsection
