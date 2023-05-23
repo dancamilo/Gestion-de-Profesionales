7d23bef29a1055dde3affc345ae7b0ee10b48c83
LinaCruzM <linacruzmolina@gmail.com>
2023-05-23 09:26:08 -0500

    Añadir/editar empresas y profesionales

--- resources/views/profesionales/create2.blade.php
+++ resources/views/profesionales/create2.blade.php
@@ -1,4 +1,31 @@
 @extends('layouts.app')
 @section('content')
-<h1>Añadir profesional 2</h1>
+<div class=" w-100v mt-3">
+    <div class="create-card w-25 me-4 p-4">
+        <h3>Añadir profesional</h3>
+        <hr width="80%">
+        <h4 class="pt-3">Redes sociales</h4>
+        <label class="w-100 my-3">
+            <input type="radio" class="me-2" name="" id="" autocomplete="off"><i class="fa-brands fa-facebook"></i>Facebook
+        </label>
+        <label class="w-100 my-3">
+            <input type="radio" class="me-2" name="" id="" autocomplete="off"> <i class="fa-brands fa-linkedin"></i>LinkedIn
+        </label>
+        <label class="w-100 my-3">
+            <input type="radio" class="me-2" name="" id="" autocomplete="off"><i class="fa-brands fa-square-instagram"></i>Instagram
+        </label>
+        <label class="w-100 my-3">
+            <input type="radio" class="me-2" name="" id="" autocomplete="off"><i class="fa-brands fa-youtube"></i>Youtube
+        </label>
+        <label class="w-100 my-3">
+            <input type="radio" class="me-2" name="" id="" autocomplete="off"> <i class="fa-brands fa-github"></i>Github
+        </label>
+        <label class="w-100 my-3">
+            <input type="radio" class="me-2" name="" id="" autocomplete="off"><i class="fa-brands fa-twitter"></i>Twitter
+        </label>
+        <label class="w-100 my-3">
+            <input type="radio" class="me-2" name="" id="" autocomplete="off"><i class="fa-brands fa-tiktok"></i>Tik tokk
+        </label>
+    </div>
+</div>
 @endsection
