9c0d36c5a34ff985a4e9877d453069e524198580
LinaCruzM <linacruzmolina@gmail.com>
2023-05-24 08:14:18 -0500

    Modulo empresas terminado

--- resources/views/empresas/index.blade.php
+++ resources/views/empresas/index.blade.php
@@ -4,132 +4,133 @@
 <div class="container">
     <div class="row mt-5">
         <h2>Empresas</h2><hr>
-
-            <div class="col-sm-2 text-dark">
-                <p>Ordernar por: </p>
-            </div>
-            <div class="col-sm-4">
-                <select class="form-select bg-white">
-                    <option></option>
-                    <option></option>
-                    <option></option>
-                    <option></option>
-                </select>
-            </div>
+        <div class="col-sm-2 text-dark">
+            <p>Ordernar por: </p>
         </div>
-    <div class="row">
-        <div class="col-sm-12 col-md-6">
-            <div class="mt-5">
-            <div class="card text-white bg-light mt-5">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4 class="card-title">Empresa 1</h4>
-                            <p class="card-text text-dark">Direccion:</p>
-                            <p class="card-text text-dark">Rut:</p>
-                        </div>
-                        <div class="col-sm-3 text-dark">
-                            <img src="https://cdn.pixabay.com/photo/2018/10/01/10/15/male-3715813_640.jpg" class="rounded-circle" style="width: 120px;"
-                            alt="Avatar" />
-                        </div>
-                    </div><hr>
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4 class="card-title">Nombre contacto empresa:</h4>
-                            <p class="card-text text-dark">Cargo:</p>
-                            <p class="card-text text-dark">Uniempresarial@uniempresarial.edu.co</p>
-                        </div>
-                        <div class="col-sm-3 text-dark">
-                            <img src="https://cdn.pixabay.com/photo/2018/10/21/13/01/toy-3762868_640.jpg" class="rounded-circle" style="width: 120px;"
-                            alt="Avatar" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card text-white bg-light mt-5">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4 class="card-title">Empresa 3</h4>
-                            <p class="card-text text-dark">Direccion:</p>
-                            <p class="card-text text-dark">Rut:</p>
-                        </div>
-                        <div class="col-sm-3 text-dark">
-                            <img src="https://cdn.pixabay.com/photo/2018/10/19/05/12/naruto-3757871_640.jpg" class="rounded-circle" style="width: 120px;"
-                            alt="Avatar" />
-                        </div>
-                    </div><hr>
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4 class="card-title">Nombre contacto empresa:</h4>
-                            <p class="card-text text-dark">Cargo:</p>
-                            <p class="card-text text-dark">Uniempresarial@uniempresarial.edu.co</p>
-                        </div>
-                        <div class="col-sm-3 text-dark">
-                            <img src="https://cdn.pixabay.com/photo/2018/10/20/03/26/young-3760179_640.jpg" class="rounded-circle" style="width: 120px;"
-                            alt="Avatar" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            </div>
-        </div>
-        <div class="col-sm-12 col-md-6 ">
-                <div class="card text-white bg-light mt-5">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-9">
-                                <h4 class="card-title">Empresa 2</h4>
-                                <p class="card-text text-dark">Direccion:</p>
-                                <p class="card-text text-dark">Rut:</p>
-                            </div>
-                            <div class="col-sm-3 text-dark">
-                                <img src="https://cdn.pixabay.com/photo/2018/10/22/13/05/ninja-3765284_640.jpg" class="rounded-circle" style="width: 120px;"
-                                alt="Avatar" />
-                            </div>
-                        </div><hr>
-                        <div class="row">
-                            <div class="col-sm-9">
-                                <h4 class="card-title">Nombre contacto empresa:</h4>
-                                <p class="card-text text-dark">Cargo:</p>
-                                <p class="card-text text-dark">Uniempresarial@uniempresarial.edu.co</p>
-                            </div>
-                            <div class="col-sm-3 text-dark">
-                                <img src="https://cdn.pixabay.com/photo/2018/10/20/03/32/ninja-3760183_640.jpg" class="rounded-circle" style="width: 120px;"
-                                alt="Avatar" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card text-white bg-light mt-5">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-9">
-                                <h4 class="card-title">Empresa 4</h4>
-                                <p class="card-text text-dark">Direccion:</p>
-                                <p class="card-text text-dark">Rut:</p>
-                            </div>
-                            <div class="col-sm-3 text-dark">
-                                <img src="https://cdn.pixabay.com/photo/2018/10/20/00/26/ninja-3760010_640.jpg" class="rounded-circle" style="width: 120px;"
-                                alt="Avatar" />
-                            </div>
-                        </div><hr>
-                        <div class="row">
-                            <div class="col-sm-9">
-                                <h4 class="card-title">Nombre contacto empresa:</h4>
-                                <p class="card-text text-dark">Cargo:</p>
-                                <p class="card-text text-dark">Uniempresarial@uniempresarial.edu.co</p>
-                            </div>
-                            <div class="col-sm-3 text-dark">
-                                <img src="https://cdn.pixabay.com/photo/2018/10/20/03/30/ninja-3760182_640.jpg" class="rounded-circle" style="width: 120px;"
-                                alt="Avatar" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            
+        <div class="col-sm-4">
+            <select class="form-select bg-white">
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+            </select>
         </div>
     </div>
+    <div class="d-flex">
+        <div class="mt-5">
+            <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0">
+                <div class="ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4">
+                                <h3>Empresa 1</h3>
+                                <h5>Dirección</h5>
+                                <h5>Rut:12345678</h5>
+                            </div>
+                            <div>
+                                <img class="rounded-circle mt-4 h-50" src="{{asset('img/Empresa1.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 mt-n2">
+                        <div class="ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4">
+                                <h3>Contacto empresa</h3>
+                                <h5>Cargo</h5>
+                                <a href="#">nombre@uniempresarial.edu.co</a>   
+                            </div>
+                            <div>
+                                <img class="rounded-circle mt-4 h-50" src="{{asset('img/User1.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+            </div>
+            <div class="mt-5 ms-5">
+                <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0">
+                    <div class="ms-0 border-left2 d-flex position-relative">
+                        <div class="p-4">
+                            <h3>Empresa 2</h3>
+                            <h5>Dirección</h5>
+                            <h5>Rut:12345678</h5>
+                        </div>
+                        <div>
+                            <img class="rounded-circle mt-4 h-50" src="{{asset('img/Empresa1.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <hr class="my-2">
+                </div>
+                <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 mt-n2">
+                    <div class="ms-0 border-left2 d-flex position-relative">
+                        <div class="p-4">
+                            <h3>Contacto empresa</h3>
+                            <h5>Cargo</h5>
+                            <a href="#">nombre@uniempresarial.edu.co</a>   
+                        </div>
+                        <div>
+                            <img class="rounded-circle mt-4 h-50" src="{{asset('img/User2.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <hr class="my-2">
+                </div>
+            </div>
+        </div>
+        <div class="d-flex">
+            <div class="mt-5">
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0">
+                        <div class="ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4">
+                                <h3>Empresa 3</h3>
+                                <h5>Dirección</h5>
+                                <h5>Rut:12345678</h5>
+                            </div>
+                            <div>
+                                <img class="rounded-circle mt-4 h-50" src="{{asset('img/Empresa1.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 mt-n2">
+                        <div class="ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4">
+                                <h3>Contacto empresa</h3>
+                                <h5>Cargo</h5>
+                                <a href="#">nombre@uniempresarial.edu.co</a>   
+                            </div>
+                            <div>
+                                <img class="rounded-circle mt-4 h-50" src="{{asset('img/User3.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+            </div>
+            <div class="mt-5 ms-5">
+                <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0">
+                    <div class="ms-0 border-left2 d-flex position-relative">
+                        <div class="p-4">
+                            <h3>Empresa 4</h3>
+                            <h5>Dirección</h5>
+                            <h5>Rut:12345678</h5>
+                        </div>
+                        <div>
+                            <img class="rounded-circle mt-4 h-50" src="{{asset('img/Empresa4.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <hr class="my-2">
+                </div>
+                <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 mt-n2">
+                    <div class="ms-0 border-left2 d-flex position-relative">
+                        <div class="p-4">
+                            <h3>Contacto empresa</h3>
+                            <h5>Cargo</h5>
+                            <a href="#">nombre@uniempresarial.edu.co</a>   
+                        </div>
+                        <div>
+                            <img class="rounded-circle mt-4 h-50" src="{{asset('img/User2.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <hr class="my-2">
+                </div>
+            </div>
+        </div>
 </div>
 @endsection
