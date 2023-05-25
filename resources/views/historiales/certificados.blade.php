e9837d4517f442128d8e53b35fac8c32bc52e400
LinaCruzM <109240037+LinaCruzM@users.noreply.github.com>
2023-05-25 15:26:27 -0600

    Form  Cursos

--- resources/views/historiales/certificados.blade.php
+++ resources/views/historiales/certificados.blade.php
@@ -1,90 +1,105 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="row mt-5">
-            <h2>Historial certificados</h2><hr>
-                    <div class="row">
-                        <div class="col-sm-2 text-dark">
-                            <p>Ordernar por:</p>
-                        </div>
-                        <div class="col-sm-4">
-                            <select class="form-select bg-white">
-                                <option></option>
-                                <option></option>
-                                <option></option>
-                                <option></option>
-                            </select>
-                        </div>
-                    </div>
-        </div>
+<div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-6">
-            <div class="mt-5">   
-                <div class="card text-white bg-light mt-5">
-                  <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4 class="card-title text-danger">Certificados 123456</h4>
-                            <h5 class="card-text text-dark">curso 1</h5>
-                            <p class="card-text text-dark">Emitido el: 22/12/2022</p>
-                            <p class="card-text text-dark">Emitido por: Nombre usuario</p>
-                        </div>
+            <div class="mt-5">
+                <h2>Historial Certificados</h2><hr>
+                <div class="row mt-5">
+                    <div class="col-sm-2 text-dark p-0">
+                        <p class="ms-2 me-1">Ordernar por: </p>
                     </div>
-                  </div>
-                </div>
-                <div class="card text-white bg-light mt-5">
-                  <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4 class="card-title text-danger">Certificados 123456</h4>
-                            <h5 class="card-text text-dark">curso 1</h5>
-                            <p class="card-text text-dark">Emitido el: 22/12/2022</p>
-                            <p class="card-text text-dark">Emitido por: Nombre usuario</p>
-                        </div>
+                    <div class="col-sm-7 p-0">
+                        <select class="form-select bg-white">
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                        </select>
                     </div>
-                  </div>
-                </div>
-                <div class="card text-white bg-light mt-5">
-                  <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4 class="card-title text-danger">Certificados 123456</h4>
-                            <h5 class="card-text text-dark">curso 1</h5>
-                            <p class="card-text text-dark">Emitido el: 22/12/2022</p>
-                            <p class="card-text text-dark">Emitido por: Nombre usuario</p>
-                            
-                        </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="card text-white bg-light mt-5">
-                  <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h4 class="card-title text-danger">Certificados 123456</h4>
-                            <h5 class="card-text text-dark">curso 1</h5>
-                            <p class="card-text text-dark">Emitido el: 22/12/2022</p>
-                            <p class="card-text text-dark">Emitido por: Nombre usuario</p>
-                        </div>
-                    </div>
-                  </div>
                 </div>
             </div>
         </div>
-            <div class="col-sm-12 col-md-6 ">
-                
-                <div class="container mx-3 bg-gray text-white p-5 mt-5">
-                <div class="row">
-                        <div class="col-md-6">
-                            <button type="submit" class="btn btn-primary">Descargar</button>
-                        </div>
-                        <div class="col-md-6">
-                            <button type="submit" class="btn btn-primary">Imprimir</button>  
-                        </div>
-                    
-                </div>
-            </div>
-        </div>   
     </div>
-</div>
+    <div class="row">
+        <div class="d-flex">
+            <div class="col-sm-12 col-md-6">
+                <div class="d-flex mt-4 ms-1 my-3">
+                    <div class="text-dark">
+                        <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1" checked>
+                        <label class="form-check-label" for="radio1"></label>
+                    </div>
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 ms-1" id="radio1">
+                        <div class=" row ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4 w-100 me-5 col-sm-9">
+                                <h3>Curso 1</h3>
+                                <h5>Profesional encargado</h5>
+                                <div class="progress w-100">
+                                    <div class="progress-bar bg-success" style="width:100%">100%</div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+                </div>
+                <div class="d-flex mt-4 ms-1 my-3">
+                    <div class="text-dark">
+                        <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option1">
+                        <label class="form-check-label" for="radio1"></label>
+                    </div>
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 ms-1" id="radio2">
+                        <div class=" row ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4 w-100 me-5 col-sm-9">
+                                <h3>Curso 2</h3>
+                                <h5>Profesional encargado</h5>
+                                <div class="progress w-100">
+                                    <div class="progress-bar bg-success" style="width:100%">100%</div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+                </div>
+                <div class="d-flex mt-4 ms-1 my-3">
+                    <div class="text-dark">
+                        <input type="radio" class="form-check-input" id="radio3" name="optradio" value="option1">
+                        <label class="form-check-label" for="radio1"></label>
+                    </div>
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 ms-1" id="radio3">
+                        <div class=" row ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4 w-100 me-5 col-sm-9">
+                                <h3>Curso 4</h3>
+                                <h5>Profesional encargado</h5>
+                                <div class="progress w-100">
+                                    <div class="progress-bar bg-success" style="width:100%">100%</div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+                </div>
+                <div class="d-flex mt-4 ms-1 my-3">
+                    <div class="text-dark">
+                        <input type="radio" class="form-check-input" id="radio4" name="optradio" value="option1" >
+                        <label class="form-check-label" for="radio1"></label>
+                    </div>
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 ms-1" id="radio4">
+                        <div class=" row ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4 w-100 me-5 col-sm-9">
+                                <h3>Curso 6</h3>
+                                <h5>Profesional encargado</h5>
+                                <div class="progress w-100">
+                                    <div class="progress-bar bg-success" style="width:100%">100%</div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+                </div> 
+            </div>
+            <div>
+                
+            </div>
+        </div>
+</div>  
 @endsection
---
+++ resources/views/historiales/empresas/index.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+            <div class="mt-5">
+                <h2>Historial Empresas</h2><hr>
+                <div class="row mt-5">
+                    <div class="col-sm-2 text-dark p-0">
+                        <p class="ms-2 me-1">Ordernar por: </p>
+                    </div>
+                    <div class="col-sm-7 p-0">
+                        <select class="form-select bg-white">
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex">
+        <div class="mt-5">
+            <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0">
+                <div class="ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4">
+                                <h3>Empresa 1</h3>
+                                <h5>Dirección</h5>
+                                <h5>Rut:12345678</h5>
+                            </div>
+                            <div>
+                                <img class="rounded-circle mt-4 h-50" src="{{asset('img/Empresa1.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 mt-n2">
+                        <div class="ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4">
+                                <h3>Contacto empresa</h3>
+                                <h5>Cargo</h5>
+                                <a href="#">nombre@uniempresarial.edu.co</a>   
+                            </div>
+                            <div>
+                                <img class="rounded-circle mt-4 h-50" src="{{asset('img/User1.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+            </div>
+            <div class="mt-5 ms-5">
+                <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0">
+                    <div class="ms-0 border-left2 d-flex position-relative">
+                        <div class="p-4">
+                            <h3>Empresa 2</h3>
+                            <h5>Dirección</h5>
+                            <h5>Rut:12345678</h5>
+                        </div>
+                        <div>
+                            <img class="rounded-circle mt-4 h-50" src="{{asset('img/Empresa1.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <hr class="my-2">
+                </div>
+                <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 mt-n2">
+                    <div class="ms-0 border-left2 d-flex position-relative">
+                        <div class="p-4">
+                            <h3>Contacto empresa</h3>
+                            <h5>Cargo</h5>
+                            <a href="#">nombre@uniempresarial.edu.co</a>   
+                        </div>
+                        <div>
+                            <img class="rounded-circle mt-4 h-50" src="{{asset('img/User2.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <hr class="my-2">
+                </div>
+            </div>
+        </div>
+        <div class="d-flex">
+            <div class="mt-5">
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0">
+                        <div class="ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4">
+                                <h3>Empresa 3</h3>
+                                <h5>Dirección</h5>
+                                <h5>Rut:12345678</h5>
+                            </div>
+                            <div>
+                                <img class="rounded-circle mt-4 h-50" src="{{asset('img/Empresa1.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+                    <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 mt-n2">
+                        <div class="ms-0 border-left2 d-flex position-relative">
+                            <div class="p-4">
+                                <h3>Contacto empresa</h3>
+                                <h5>Cargo</h5>
+                                <a href="#">nombre@uniempresarial.edu.co</a>   
+                            </div>
+                            <div>
+                                <img class="rounded-circle mt-4 h-50" src="{{asset('img/User3.jpg')}}" alt="">
+                            </div>
+                        </div>
+                        <hr class="my-2">
+                    </div>
+            </div>
+            <div class="mt-5 ms-5">
+                <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0">
+                    <div class="ms-0 border-left2 d-flex position-relative">
+                        <div class="p-4">
+                            <h3>Empresa 4</h3>
+                            <h5>Dirección</h5>
+                            <h5>Rut:12345678</h5>
+                        </div>
+                        <div>
+                            <img class="rounded-circle mt-4 h-50" src="{{asset('img/Empresa4.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <hr class="my-2">
+                </div>
+                <div class="collapsible-div2 position-relative text-dark rounded-0 p-4 pb-0 mt-n2">
+                    <div class="ms-0 border-left2 d-flex position-relative">
+                        <div class="p-4">
+                            <h3>Contacto empresa</h3>
+                            <h5>Cargo</h5>
+                            <a href="#">nombre@uniempresarial.edu.co</a>   
+                        </div>
+                        <div>
+                            <img class="rounded-circle mt-4 h-50" src="{{asset('img/User2.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <hr class="my-2">
+                </div>
+            </div>
+        </div>
+</div>
+@endsection
